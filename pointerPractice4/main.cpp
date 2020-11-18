3434522a426eeec246482888c9ff5f9d34ab9a49
Jason <jkasari@outlook.com>
2020-11-17 20:38:59 -0800

    add displayArr function

--- pointerPractice4/main.cpp
+++ pointerPractice4/main.cpp
@@ -2,9 +2,16 @@
 
 using namespace std;
 
+void displayArr(const int32_t arrSize, const int32_t* arr) {
+  for(int i = 0; i < arrSize; ++i) {
+    cout << arr[i] << endl;
+  }
+}
+
 int main() {
-
-  cout << "Hello world\n";
-
+  const int32_t arrSize = 5;
+  const int32_t* arr = new int32_t[arrSize];
+  displayArr(arrSize, arr);
   return 0;
+
 }
