41dafd363ee75f9fc7e198b73f46da2dddea8ea2
Jason <jkasari@outlook.com>
2020-11-17 20:47:46 -0800

    add incrementAtIndex function

--- pointerPractice4/main.cpp
+++ pointerPractice4/main.cpp
@@ -8,9 +8,16 @@ void displayArr(const int32_t arrSize, const int32_t* arr) {
   }
 }
 
+void incrementAtIndex(const int32_t arrIndex, int32_t* arr) {
+  ++arr[arrIndex];
+}
+
 int main() {
   const int32_t arrSize = 5;
-  const int32_t* arr = new int32_t[arrSize];
+  int32_t* arr = new int32_t[arrSize];
+  displayArr(arrSize, arr);
+  incrementAtIndex(3, arr);
+  cout << "---------------------" << endl;
   displayArr(arrSize, arr);
   return 0;
 
